6f48e957cecf55b1624d0a593947fb812d522d5b
Bosco <bosco11jico100@gmail.com>
2020-04-30 19:28:14 -0600

    g
    g

--- viruss/src/viruss/controller/InicioController.java
+++ viruss/src/viruss/controller/InicioController.java
@@ -44,10 +44,17 @@ public class InicioController extends Controller implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //crearCartas();
-        Image p = new Image("viruss/resources/marco");
-        ImageView  w = new ImageView(p);
-        
+        crearCartas();
+        int cont=0;
+        for(Carta carta : lista)
+        {
+            
+            if(cont<3)
+            {
+                hboxMasoJug.getChildren().add(cont, carta);
+                cont++;
+            }
+        }
     }   
 
     @Override
@@ -60,73 +67,73 @@ public class InicioController extends Controller implements Initializable {
         for (int i = 0; i < 68; i++) {//organos
             if (i < 21) {
                 if (i < 5) {
-                    lista.add(new Carta("Organos", 1, 50, 50));//corazon
+                    lista.add(new Carta("viruss/recursos/CORAZON.jpg","Organos", 1, 50, 50));//corazon
                 }
                 if (i >= 5 && i < 10) {
-                    lista.add(new Carta("Organos", 2, 50, 50));//estomago
+                    lista.add(new Carta("viruss/recursos/ESTOMAGO.jpg","Organos", 2, 50, 50));//estomago
                 }
                 if (i >= 10 && i < 15) {
-                    lista.add(new Carta("Organos", 3, 50, 50));//cerebor
+                    lista.add(new Carta("viruss/recursos/CEREBRO.jpg","Organos", 3, 50, 50));//cerebor
                 }
                 if (i >= 15 && i < 20) {
-                    lista.add(new Carta("Organos", 4, 50, 50));//hueso
+                    lista.add(new Carta("viruss/recursos/HUESO.jpg","Organos", 4, 50, 50));//hueso
                 }
                 if (i >= 20 && i < 21) {
-                    lista.add(new Carta("Organos", 5, 50, 50));//comodin
+                    lista.add(new Carta("viruss/recursos/CUERPO.jpg","Organos", 5, 50, 50));//comodin
                 }
             }
             if(i>=21 && i<38)//virus
             {
                 if (i < 25) {
-                    lista.add(new Carta("Virus", 1, 50, 50));//ROJIVIRUS
+                    lista.add(new Carta("viruss/recursos/ROJIVURUS.jpg","Virus", 1, 50, 50));//ROJIVIRUS
                 }
                 if (i >= 25 && i < 29) {
-                    lista.add(new Carta("Virus", 2, 50, 50));//VERDON EL VIRUS
+                    lista.add(new Carta("viruss/recursos/VERDON EL VIRUS.jpg","Virus", 2, 50, 50));//VERDON EL VIRUS
                 }
                 if (i >= 29 && i < 33) {
-                    lista.add(new Carta("Virus", 3, 50, 50));//VIRUBLU
+                    lista.add(new Carta("viruss/recursos/VIRUBLU.jpg","Virus", 3, 50, 50));//VIRUBLU
                 }
                 if (i >= 33 && i < 37) {
-                    lista.add(new Carta("Virus", 4, 50, 50));//YELLVIRUS
+                    lista.add(new Carta("viruss/recursos/YELLVIRUS.jpg","Virus", 4, 50, 50));//YELLVIRUS
                 }
                 if (i >= 37 && i < 38) {
-                    lista.add(new Carta("Virus", 5, 50, 50));//COVID-19
+                    lista.add(new Carta("viruss/recursos/COVID-19.jpg","Virus", 5, 50, 50));//COVID-19
                 }
             }
             if(i>=38 && i<58)//medicinas
             {
                 if (i < 42) {
-                    lista.add(new Carta("Medicinas", 1, 50, 50));//BOTIQUIN
+                    lista.add(new Carta("viruss/recursos/BOTIQUIN.jpg","Medicinas", 1, 50, 50));//BOTIQUIN
                 }
                 if (i >= 42 && i < 46) {
-                    lista.add(new Carta("Medicinas", 2, 50, 50));//VACUNA
+                    lista.add(new Carta("viruss/recursos/VACUNA.jpg","Medicinas", 2, 50, 50));//VACUNA
                 }
                 if (i >= 46 && i < 50) {
-                    lista.add(new Carta("Medicinas", 3, 50, 50));//ANTISUERO
+                    lista.add(new Carta("viruss/recursos/ANTISUERO.jpg","Medicinas", 3, 50, 50));//ANTISUERO
                 }
                 if (i >= 50 && i < 54) {
-                    lista.add(new Carta("Medicinas", 4, 50, 50));//CURITAS
+                    lista.add(new Carta("viruss/recursos/CURITAS.jpg","Medicinas", 4, 50, 50));//CURITAS
                 }
                 if (i >= 54 && i < 58) {
-                    lista.add(new Carta("Medicinas", 5, 50, 50));//HOSPITAL
+                    lista.add(new Carta("viruss/recursos/HOSPITAL.jpg","Medicinas", 5, 50, 50));//HOSPITAL
                 }
             }
             if(i>=58 && i<58)//tratamientos
             {
                 if (i < 60) {
-                    lista.add(new Carta("Tratamientos", 1, 50, 50));//Transplante
+                    lista.add(new Carta("viruss/recursos/TRANSPLANTE.jpg","Tratamientos", 1, 50, 50));//Transplante
                 }
                 if (i >= 60 && i < 63) {
-                    lista.add(new Carta("Tratamientos", 2, 50, 50));//Ladrón de órganos
+                    lista.add(new Carta("viruss/recursos/LADRON DE ORGANOS.jpg","Tratamientos", 2, 50, 50));//Ladrón de órganos
                 }
                 if (i >= 63 && i < 66) {
-                    lista.add(new Carta("Tratamientos", 3, 50, 50));//Contagio
+                    lista.add(new Carta("viruss/recursos/CONTAGIO.jpg","Tratamientos", 3, 50, 50));//Contagio
                 }
                 if (i >= 66 && i < 67) {
-                    lista.add(new Carta("Tratamientos", 4, 50, 50));//Guante de látex
+                    lista.add(new Carta("viruss/recursos/GUANTE DE LATEX.jpg","Tratamientos", 4, 50, 50));//Guante de látex
                 }
                 if (i >= 67 && i < 68) {
-                    lista.add(new Carta("Tratamientos", 5, 50, 50));//Error médico
+                    lista.add(new Carta("viruss/recursos/ERROR MEDICO.jpg","Tratamientos", 5, 50, 50));//Error médico
                 }
             }
         }
--- viruss/src/viruss/model/Carta.java
+++ viruss/src/viruss/model/Carta.java
@@ -1,5 +1,7 @@
 package viruss.model;
 
+
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
@@ -10,108 +12,110 @@ public class Carta extends ImageView{
     private int color;
     private int altura;
     private int ancho;
+    public Image img;
     
-    public Carta(String tipoCarta,int color,int altura,int ancho){
+    public Carta(String img,String tipoCarta,int color,int altura,int ancho){
         this.setFitHeight(altura);
         this.setFitWidth(ancho);
         this.tipoCarta = tipoCarta;
         this.color = color;
-        
+        this.img = new Image(img);
         if(tipoCarta.equals("Virus"))
         {
             
             if(color == 1)//ROJIVIRUS
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 2)//VERDON EL VIRUS
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 3)//VIRUBLU
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 4)//YELLVIRUS
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 5)//COVID-19
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
         }
         if(tipoCarta.equals("Tratamientos"))
         {
             if(color == 1)//Transplante
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 2)//Ladrón de órganos
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 3)//Contagio
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 4)//Guante de látex
             {
-                //this.setImage("");
+               this.setImage(this.img);
             }
             if(color == 5)//Error médico
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
         }
         if(tipoCarta.equals("Organos"))
         {
             if(color == 1)//corazon
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 2)//estomago
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 3)//cerebro
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 4)//hueso
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 5)//comodin
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
         }
         if(tipoCarta.equals("Medicinas"))
         {
             if(color == 1)
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 2)
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 3)
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 4)
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
             if(color == 5)
             {
-                //this.setImage("");
+                this.setImage(this.img);
             }
         }
         this.setOnMouseClicked(ke->{
-            System.out.println("Hola");
+            System.out.println(this.color);
+            System.out.println(this.tipoCarta);
         });
     }
     

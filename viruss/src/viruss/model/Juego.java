aa9c935e23e3dd7b8fece2165ce9e43b781ff546
Gus2730 <gus2730@gmail.com>
2020-05-11 17:02:47 -0600

    rferw
    werwq

---
+++ viruss/src/viruss/model/Juego.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viruss.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author cfugu
+ */
+public class Juego {
+    List jugadores;
+    int turno;
+    List mazo;
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public List getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public List getMazo() {
+        return mazo;
+    }
+
+    public void setMazo(List mazo) {
+        this.mazo = mazo;
+    }
+    
+    
+}
---
+++ viruss/src/viruss/model/Jugador.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viruss.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author cfugu
+ */
+public class Jugador {
+    String nickname;
+    String ip;
+    List mazo1;
+    List mazo2;
+
+    public Jugador(String nickname, String ip) {
+        this.nickname = nickname;
+        this.ip = ip;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public List getMazo1() {
+        return mazo1;
+    }
+
+    public void setMazo1(List mazo1) {
+        this.mazo1 = mazo1;
+    }
+
+    public List getMazo2() {
+        return mazo2;
+    }
+
+    public void setMazo2(List mazo2) {
+        this.mazo2 = mazo2;
+    }
+    
+}
+
+

80d03f7cdc62167937bf75d9af5e14051d7651ec
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-05-11 19:42:34 -0600

    cm

--- viruss/src/viruss/model/Conexion.java
+++ viruss/src/viruss/model/Conexion.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 
 public class Conexion
 {
-    private final int PUERTO = 44440; //Puerto para la conexión
+    private final int PUERTO = 5000; //Puerto para la conexión
     private final String HOST = "localhost"; //Host para la conexión
     protected String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor
     protected ServerSocket ss; //Socket del servidor
